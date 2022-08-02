55496642389fa5afc7126db432d6456341057b36
Abhinav Ashok <abhilenovo55@gmail.com>
2022-08-02 18:04:32 +0530

    background image changed, some colors chaged

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+$("#submit-btn").click(function() {
+    alert("Thank you, I will replay you soon !")
+ });
